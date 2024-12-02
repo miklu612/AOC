11e372ad3f2c66bed69a01a5ef96072b0307716d
robo <robo@mail>
2024-12-02 15:24:16 +0200

    Got the second part working

--- src/day2/main.cpp
+++ src/day2/main.cpp
@@ -3,65 +3,105 @@
 #include<string>
 #include<optional>
 #include<array>
+#include<vector>
+#include<algorithm>
 
 
 int main() {
 
+    try {
     std::ifstream file("input.txt");
     if(!file.good()) {
         std::cout << "Failed to read input\n";
         return -1;
     }
 
+
+    int answer_p1 = 0;
+
     std::string line;
-    int valid_lines = 0;
     while(std::getline(file, line)) {
-        // Not the best solution to fix the last digit not being check, but it works.
+        std::vector<int> numbers;
+        std::string current;
         line += " ";
-        for(bool increasing : std::array<bool, 2> {false, true}) {
-            int previous = 0;
-            std::string current = "";
-            int faults = 0;
-            for(char character : line) {
-                if(character == ' ') {
-                    if(previous == 0) {
-                        previous = std::stoi(current);
-                    }
-                    else {
-                        int number = std::stoi(current);
-                        if(std::abs(previous - number) > 3 || previous == number) {
-                            std::cout << "Failed: " << number << " " << previous << "\n";
-                            std::cout << "Reason: Difference too large\n";
-                            faults += 1;
-                        }
-                        else if(increasing && previous > number) {
-                            std::cout << "Failed: " << number << " " << previous << "\n";
-                            std::cout << "Reason: Expected increased value\n";
-                            faults += 1;
-                        }
-                        else if(!increasing && previous < number) {
-                            std::cout << "Failed: " << number << " " << previous << "\n";
-                            std::cout << "Reason: Expected decreased value\n";
-                            faults += 1;
-                        }
-                        previous = number;
-                    }
-                    current.clear();
-                }
-                else {
-                    current.push_back(character);
-                }
+        for(char character : line) {
+            if(character == ' ') {
+                numbers.push_back(std::stoi(current));
+                current.clear();
             }
-            if(faults == 0) {
-                valid_lines += 1;
+            else {
+                current += character;
+            }
+        }
+
+
+        bool exit = false;
+        for(bool growing : std::array<bool, 2> {false, true}) {
+            if(exit) {
                 break;
             }
+            std::vector<int> mistakes = {0};
+            int i = -1;
+            do {
+                std::vector<int> filtered;
+                if(i < mistakes.size() && i >= 0) {
+                    std::copy(numbers.begin(), std::next(numbers.begin(), mistakes[i]), std::back_inserter(filtered));
+                    std::copy(std::next(numbers.begin(), mistakes[i]+1), numbers.end(), std::back_inserter(filtered));
+                }
+                else {
+                    filtered = numbers;
+                }
+                std::cout << "Filtered: ";
+                for(int n : filtered) {
+                    std::cout << n << " ";
+                }
+                std::cout << "\n";
+                int faults = 0;
+                for(size_t j = 1 ; j < filtered.size() ; j++) {
+                    if(growing && filtered[j-1] > filtered[j]) {
+                        if(std::find(mistakes.begin(), mistakes.end(), j) == mistakes.end()) {
+                            mistakes.push_back(j);
+                        }
+                        faults += 1;
+                    }
+                    else if(!growing && filtered[j-1] < filtered[j]) {
+                        if(std::find(mistakes.begin(), mistakes.end(), j) == mistakes.end()) {
+                            mistakes.push_back(j);
+                        }
+                        faults += 1;
+                    }
+                    else if(filtered[j-1] == filtered[j] || std::abs(filtered[j-1] - filtered[j]) > 3) {
+                        if(std::find(mistakes.begin(), mistakes.end(), j) == mistakes.end()) {
+                            mistakes.push_back(j);
+                        }
+                        faults += 1;
+                    }
+                }
+                i += 1;
+                if(faults == 0) {
+                    answer_p1 += 1;
+                    exit = true;
+                    std::cout << "Pass\n";
+                    break;
+                }
+            } while(i < mistakes.size());
+            std::cout << "Mistakes:\n";
+            for(int mistake : mistakes) {
+                std::cout << mistake << " ";
+            }
+            std::cout << "\n";
         }
     }
 
-    std::cout << "Answer: " << valid_lines << "\n";
+    std::cout << answer_p1 << "\n";
+
 
     file.close();
+    }
+
+    catch(std::exception e) {
+        std::cout << e.what() << "\n";
+    }
 
     return 0;
 
