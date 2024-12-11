b7750853ca55ade2e3203d18af9deca117b85d28
robo <robo@mail>
2024-12-11 16:44:02 +0200

    Optimization

--- src/day11/main.cpp
+++ src/day11/main.cpp
@@ -1,10 +1,43 @@
 #include<iostream>
+#include<map>
 #include"../util/file_io.hpp"
 #include"../util/string_util.hpp"
 
-
 using IntType = unsigned long long;
+const int cache_depth = 3;
 
+
+IntType solve(IntType number, int depth=0, int limit=0) {
+    if(depth >= limit) {
+        return 1;
+    }
+    else if(number == 0) {
+        if(depth+4 < limit) {
+            return 
+                solve(2, depth+4, limit) +
+                solve(0, depth+4, limit) +
+                solve(2, depth+4, limit) +
+                solve(4, depth+4, limit);
+        }
+        else {
+            return solve(1, depth+1, limit);
+        }
+    }
+    else if(number >= 10){
+        // Weird math stuff that I figured out by messing with logarithms.
+        const auto power = (IntType) std::floor(std::log10(number));
+        if(power % 2 == 1) {
+            const IntType modulo_op = std::pow(10, std::ceil(power / 2))*10;
+            const IntType lower = number % modulo_op;
+            const IntType higher = (number - lower) / (modulo_op);
+            IntType answer = 0;
+            answer += solve(higher, depth+1, limit);
+            answer += solve(lower, depth+1, limit);
+            return answer;
+        }
+    }
+    return solve(number*2024, depth+1, limit);
+}
 
 int main() {
 
@@ -19,45 +52,19 @@ int main() {
 
         std::cout << digits.size() << "\n";
 
-        unsigned long stones = 0;
-        std::vector<IntType> stack;
+        unsigned long stones_1 = 0;
         for(auto digit : digits) {
-            stack.clear();
-            stack.push_back(digit);
-
-            for(int i = 0 ; i < 25 ; i++) {
-                const auto cur_size = stack.size();
-                for(int j = 0 ; j < cur_size ; j++) {
-                    const auto& stone = stack[j];
-                    if(stone == 0) {
-                        stack.push_back(1);
-                        continue;
-                    }
-                    else if(stone >= 10){
-                        // Weird math stuff that I figured out by messing with logarithms.
-                        const auto power = (IntType) std::floor(std::log10(stone));
-                        if(power % 2 == 1) {
-                            const IntType modulo_op = std::pow(10, std::ceil(power / 2))*10;
-                            const IntType lower = stone % modulo_op;
-                            const IntType higher = (stone - lower) / (modulo_op);
-                            stack.push_back(higher);
-                            stack.push_back(lower);
-                            continue;
-                        }
-                    }
-                    stack.push_back(stone * 2024);
-                }
-                stack.erase(stack.begin(), std::next(stack.begin(), cur_size));
-                //for(auto stone : stack) {
-                //    std::cout << stone << " ";
-                //}
-                //std::cout << "\n";
-            }
-            stones += stack.size();
+            stones_1 += solve(digit, 0, 25);
         }
 
-        std::cout << "Answer 1: " << stones << "\n";
-        std::cout << "Answer 2: " << "Not implemented" << "\n";
+        unsigned long stones_2 = 0;
+        //for(auto digit : digits) {
+        //    std::cout << digit << "\n";
+        //    stones_2 += solve(digit, 0, 75);
+        //}
+
+        std::cout << "Answer 1: " << stones_1 << "\n";
+        std::cout << "Answer 2: " << stones_2 << "\n";
 
     }
     catch(std::exception e) {
